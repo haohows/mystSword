d78e072c9898d16c0464ed8e336cd76ef849fe71
黃阿皓 <faraway520@gmail.com>
2025-11-20 02:12:19 +0800

    create

---
+++ tw/gameconfig.js
@@ -0,0 +1,5 @@
+window.gameconfig = {
+  feedbackUrl: "https://forms.gle/UP6vXKMymEZoagdR9",
+  shareUrl: "https://haohows.com/mystSword/tw",
+  shareText: "我是王大熊，從小混在巷口宮廟，會點小本事的靈異調查員，某天，老朋友傳來幾張照片，說有個神秘事件需要我這種「靈界收訊良好」的體質...",
+};
